07c78b0635706023fe70d8b88ca7df897830145f
bobbiewatzen <31798845+bobbiewatzen@users.noreply.github.com>
2017-09-09 20:17:44 +0200

    Update GitTestApplication.java
    get rid of an exclamation mark

--- src/packt/jee/eclipse/git/GitTestApplication.java
+++ src/packt/jee/eclipse/git/GitTestApplication.java
@@ -2,6 +2,6 @@ package packt.jee.eclipse.git;
 
 public class GitTestApplication {
   public static void main(String[] args) {
-    System.out.println("Hello Git, from branch bug#1234 !!");
+    System.out.println("Hello Git, from branch bug#1234!");
   }
 }
